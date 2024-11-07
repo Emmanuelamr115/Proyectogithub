9b8ea8d8c9903835a9cf8539dea860b85fe4de98
Emmanuel <Emmanuel@EmmanuelPC1>
2024-11-07 07:53:23 -0600

    Versión inicial

---
+++ src/main/java/com/mycompany/holagithub/Holagithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holagithub;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class Holagithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello github!");
+    }
+}
